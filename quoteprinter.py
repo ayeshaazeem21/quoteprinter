9b105b9feba3c67cda8a2da864951ea41bf26389
ayeshaazeem21 <ayesha2006azeem@gmail.com>
2025-04-16 23:46:32 +0530

    Add quote printer script

---
+++ quoteprinter.py
@@ -0,0 +1,12 @@
+#quote-printer
+
+import random
+
+quotes = [
+    "Stay soft. It looks beautiful on you.",
+    "The only way out is through.",
+    "Progress, not perfection.",
+    "You are not behind. You're on your own path."
+]
+print("📝 Your quote of the day:")
+print(random.choice(quotes))
